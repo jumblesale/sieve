ff5a4cb68f9061bafbcade9547cbd667ee1ab7dd
jumblesale <rob@robsollars.co.uk>
2017-02-05 13:34:32 +0000

    removed everything

--- sieve.hs
+++ sieve.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 import Test.QuickCheck
+import Data.List as List
+import Data.Set as Set
 
 sieve :: Int -> [Int]
 sieve = undefined
 
-getFirstUniqueElementInList :: (Eq a) => [a] -> [a]
-getFirstUniqueElementInList = undefined
+getFirstUniqueElementInList :: (Eq a) => [a] -> [a] -> a
+-- \\ is the list difference operator apparently
+getFirstUniqueElementInList x y = head $ x List.\\ y
+prop_getFirstUniqueElementInList
+    = getFirstUniqueElementInList [2,7,11,23] [2,7,23] == 11
 
 squareGELimit :: Int -> Int -> Bool
 squareGELimit x y = x*x >= y
@@ -26,7 +31,8 @@ tests :: [(String, Bool)]
 tests = 
     [("it takes every nth element from a list", prop_dropNthApp),
      ("it is true for the square of seven greater than / equals 30", prop_squareGELimitTrueApp),
-     ("it is false for the square of seven greater than / equals 50", prop_squareGELimitFalseApp)]
+     ("it is false for the square of seven greater than / equals 50", prop_squareGELimitFalseApp),
+     ("it gets the first unique element from two lists", prop_getFirstUniqueElementInList)]
 
 testResult :: Bool
 testResult = all (\x -> snd x == True) tests
