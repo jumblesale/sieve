9f16c0da611d808eeb0b9538ae6ec9781182f41c
jumblesale <rob@robsollars.co.uk>
2017-02-05 11:51:17 +0000

    refactored method signatures to be parameterisable

--- sieve.hs
+++ sieve.hs
@@ -2,15 +2,18 @@
 
 import Test.QuickCheck
 
-allOdds :: [Int]
-allOdds = [3,5..]
+sieve :: Int -> [Int]
+sieve = undefined
+
+getFirstUniqueElementInList :: (Eq a) => [a] -> [a]
+getFirstUniqueElementInList = undefined
 
 squareGELimit :: Int -> Int -> Bool
 squareGELimit x y = x*x >= y
 prop_squareGELimitTrueApp  = True == squareGELimit 7 30
 prop_squareGELimitFalseApp = False == squareGELimit 7 50
 
-dropEveryNth :: Int -> [Int] -> [Int]
+dropEveryNth :: Int -> [a] -> [a]
 dropEveryNth n xs = case drop (n - 1) xs of
     []     -> []
     (y:ys) -> y : dropEveryNth n ys
@@ -23,7 +26,7 @@ tests :: [(String, Bool)]
 tests = 
     [("it takes every nth element from a list", prop_dropNthApp),
      ("it is true for the square of seven greater than / equals 30", prop_squareGELimitTrueApp),
-     ("it is false for the square of seven greater than / equals 30", prop_squareGELimitFalseApp)]
+     ("it is false for the square of seven greater than / equals 50", prop_squareGELimitFalseApp)]
 
 testResult :: Bool
 testResult = all (\x -> snd x == True) tests
